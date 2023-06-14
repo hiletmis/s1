c9e5d4e0ead5659675a9b9a59f95e6d961710e09
hiletmis <hayrettin@digilirapay.com>
2023-06-14 13:29:14 +0300

    axios replaced with node-fetch

--- _helpers/onesignal.js
+++ _helpers/onesignal.js
@@ -1,9 +1,25 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
 const dotenv = require('dotenv');
+
+module.exports = {
+    sendMessage
+};
+
+/**
+ * Send a push notification to a user's device using OneSignal.
+ * @param {string} playerId The user's OneSignal player ID.
+ * @param {string} message The message to send.
+ * @returns {Promise<void>}
+ * @see https://documentation.onesignal.com/reference/create-notification
+ * @see https://documentation.onesignal.com/docs/players-view-device#section-example-code-create-a-notification
+ * @see https://documentation.onesignal.com/docs/using-the-api
+ * @see https://documentation.onesignal.com/docs/onesignal-api
+ * @see https://documentation.onesignal.com/docs/onesignal-api-send-notifications
+ * 
+ **/
 
 const sendMessage = async(playerId, message) => {
     const appId = process.env.ONESIGNAL_APP_ID;
-
     const restApiKey = process.env.ONESIGNAL_REST_API_KEY;
 
     const notification = {
@@ -13,24 +29,16 @@ const sendMessage = async(playerId, message) => {
     };
 
     try {
-        const response = await axios.post(
-            'https://onesignal.com/api/v1/notifications',
-            notification, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Basic ${restApiKey}`,
-                },
-            }
-        );
-
+        const response = await fetch('https://onesignal.com/api/v1/notifications', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Basic ${restApiKey}`,
+            },
+            body: JSON.stringify(notification),
+        });
         console.log('Notification sent successfully:', response.data);
     } catch (error) {
         console.error('Failed to send notification:', error);
     }
 };
-
-// Example usage
-const playerId = process.env.ONESIGNAL_PLAYER_ID;
-const message = 'Hello, this is a test notification!';
-
-sendMessage(playerId, message);
